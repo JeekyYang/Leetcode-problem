e5a5976e876bc2c1f3bcb51d206e9f6df9116bff
jileiyang <jileiyang@creditease.cn>
2015-07-21 14:55:49 +0800

    the problem: max subarray sum

---
+++ maxSubArraySum.cpp
@@ -0,0 +1,15 @@
+int maxSubArraySum(int A[],int n)
+{
+	if(0 == n) return 0;
+	int thissum = A[0];
+	int maxsum = A[0];
+	for(int i=1;i<n;i++)
+	{
+		if(thissum <= 0 )
+			thissum = A[i];
+		else
+			thissum += A[i];
+		maxsum = max(thissum,maxsum);
+	}
+	return maxsum;
+}
