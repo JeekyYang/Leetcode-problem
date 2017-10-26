e6c4ec639e22707b2289f2cf4755e28466959089
Yang Jilei <jileiyang@creditease.cn>
2017-10-27 02:56:23 +0800

    word ladder

---
+++ wordLadder.cpp
@@ -0,0 +1,44 @@
+class Node{
+public:
+	string word;
+	int len;
+	Node(string w, int l):word(w), len(l) {}
+};
+int wordLadder(string begin, string end, vector<int> &wordList) {
+	if(begin.empty()) {
+		return 0;
+	}
+
+	queue<Node> qe;
+	qe.push(Node(begin, 1));
+
+	unordered_set<string> wordSet(wordList.begin, wordList.end());
+	map<string, bool> used;
+	used[begin] = true;
+
+	const int n = begin.size();
+	while(!qe.empty()) {
+		Node cur = qe.front();
+		if(cur.word == end) {
+			return cur.len;
+		}
+
+		string tmp = cur.word;
+		for(int i = 0; i < n; i++) {
+			for(char j = 'a'; j <= 'z'; j++) {
+				if(tmp[i] == j) {
+					continue;
+				}
+
+				tmp[i] = j;
+				if(wordSet.find(tmp) == wordSet.end() || used[tmp]) {
+					continue;
+				}
+
+				qe.push(Node(tmp, cur.len+1));
+				used[tmp] = true;
+			}
+		}
+	}
+	return 0;
+}
