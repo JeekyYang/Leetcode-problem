de95d4c6bffd3cd29ae17c35b3c787dcc00049b8
Yang Jilei <jileiyang@creditease.cn>
2017-11-02 00:37:13 +0800

    optimize

--- palindromeCuts.cpp
+++ palindromeCuts.cpp
@@ -2,33 +2,23 @@ int cuts(string s) {
 	if(s.empty()) {
 		return 0;
 	}
+
 	const int n = s.size();
-	vector<int> f(n+1, n-1);
-	f[0] = -1;
-	f[1] = 0;
+	vector<vector<bool>> check(n, vector<bool>(n, false));
+	
+	vector<int> f(n+1);
+	for(int i = 0; i < n+1; i++) {
+		f[i] = i-1;
+	}
+
 	for(int i = 2; i <= n; i++) {
-		f[i] = f[i-1] + 1;
-		for(int j = i-1; i >= 0; j--) {
-			string str = s.substr(s, j, i-j+1);
-			if(isPalindrome(str) && f[j] + 1 < f[i]) {
-			 	f[i] = f[j] + 1;
+		for(int j = i-1; j >= 0; j--) {
+			if(s[j] == s[i-1] && (i-1-j < 2 || check[j+1][i-2])) {
+				f[i] = min(f[i], f[j] + 1);
+				check[j][i-1] = true;
 			}
 		}
 	}
 	return f[n];
 }
 
-bool isPalindrome(string s) {
-	if(s.empty()) {
-		return true;
-	}
-	int first = 0, last = s.size()-1;
-	while(first < last) {
-		if(s[first] != s[last]) {
-			return false;
-		}
-		first++;
-		last--;
-	}
-	return true;
-}
