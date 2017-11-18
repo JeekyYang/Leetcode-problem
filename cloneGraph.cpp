cd6cb3d8f43fc0baca83c5060b9fcd5928bfffc2
Yang Jilei <jileiyang@creditease.cn>
2017-11-18 23:23:50 +0800

    clone graph with bfs

--- cloneGraph.cpp
+++ cloneGraph.cpp
@@ -20,3 +20,32 @@ UndirectedGraphNode *clone(UndirectedGraphNode *node, unordered_map<UndirectedGr
 	}
 	return myNode;
 }
+
+//bfs
+
+UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+	if(nullptr == node) {
+		return nullptr;
+	}
+
+	unordered_map<UndirectedGraphNode *,UndirectedGraphNode *> cloned;
+	cloned[node] = new UndirectedGraphNode(node->label);
+	queue<UndirectedGraphNode *> qe;
+	qe.push(node);
+	while(!qe.empty()) {
+		UndirectedGraphNode *cur = qe.front();
+		qe.pop();
+		for(auto nbr:cur->neighbors) {
+			if(cloned.find(nbr) != cloned.end()) {
+				cloned[cur]->neighbors.push_back(cloned[nbr]);
+			} else {
+				UndirectedGraphNode *myNeighbor = new UndirectedGraphNode(nbr->label);
+				cloned[nbr] = myNeighbor;
+				cloned[cur]->neighbors.push_back(myNeighbor);
+				qe.push(nbr);
+			}
+		}
+
+	}
+	return cloned[node];
+}
