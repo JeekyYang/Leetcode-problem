0f2338f9b0e2cbbbd7e47e335a6d1c386829a58e
Yang Jilei <jileiyang@creditease.cn>
2017-12-03 17:31:08 +0800

    optimize jump II

--- jumpII.cpp
+++ jumpII.cpp
@@ -1,27 +1,18 @@
 int jump(vector<int> &nums) {
-	if(nums.empty()) {
-		return -1;
-	}
-	if(nums.size()==1) {
-		return 0;
-	}
-	int target = nums.size()-1;
-	int most = nums[0];
-	int curMost = most;
-	int step = 1;
-	for(int i = 1; i < nums.size(); i++) {
+	if(nums.empty() || nums.size() == 1) return 0;
+
+	const int n = nums.size();
+	if(nums[0] >= n-1) return 1;
+
+	int most = nums[0], cur = most;
+	int steps = 1;
+	for(int i = 1; i < n; i++) {
 		if(i > most) {
-			step++;
-			most = curMost;
+			steps++;
+			most = cur;
 		}
-
-		if(i+nums[i] >= target) {
-			return step+1;
-		}
-
-		if(i+nums[i] > curMost) {
-			curMost = i+nums[i];
-		}
+		if(i+nums[i] >= n-1) return steps+1;
+		cur = max(cur, nums[i]+i);
 	}
-	return -1;
+	return steps;
 }
