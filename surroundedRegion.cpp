4c5dfc5e3987e54a518d78e0b92d7c089a5d0f30
Yang Jilei <jileiyang@creditease.cn>
2017-12-07 01:43:45 +0800

    add dfs solution for surrouned region

--- surroundedRegion.cpp
+++ surroundedRegion.cpp
@@ -1,3 +1,39 @@
+//dfs
+void solve(vector<vector<int>> &board) {
+	if(board.empty()) return;
+	if(board[0].empty()) return;
+
+	const int m = board.size();
+	const int n = board[0].size();
+
+	for(int i = 0; i < m; i++) {
+		dfs(board, i, 0);
+		dfs(board, i, n-1);
+	}
+	for(int j = 0; j < n; j++) {
+		 dfs(board, 0, j);
+		 dfs(board, m-1, j)
+	}
+	for(int i = 0; i < m; i++) {
+		for(int j = 0; j < n; j++) {
+			if(board[i][j] == 'o') {
+				board[i][j] = 'x';
+			} else if(board[i][j] = 'z') {
+				board[i][j] = 'o';
+			}
+		}
+	}
+}
+void dfs(vector<vector<char>> &board, int x, int y) {
+	if(board[x][y] != 'o') return;
+	board[x][y] = 'z';
+	if(x-1 >= 0) dfs(board, x-1, y);
+	if(x+1 < m) dfs(board, x+1, y);
+	if(y-1 >= 0) dfs(board, x, y-1);
+	if(y+1 < n) dfs(board, x, y+1);
+}
+
+//bfs
 class Node {
 	public:
 		int x;
