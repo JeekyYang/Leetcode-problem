dcce16efb2d0221253051323b91e5d5a5b233575
Yang Jilei <jileiyang@creditease.cn>
2017-12-14 02:11:41 +0800

    unique binary search trees

---
+++ uniqueBST.cpp
@@ -0,0 +1,11 @@
+int uniqueBST(int n) {
+	if(n <= 1) return n;
+	vector<int> f(n+1, 0);
+	f[0] = f[1] = 1;
+	for(int i = 2; i <= n; i++) {
+		for(int j = 0; j <= i+1; j++) {
+			f[i] += f[j] * f[i-1-j];
+		}
+	}
+	return f[n];
+}
